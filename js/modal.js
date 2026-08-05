74533e09d61ce276b54e5f4bc62e684e78086009
liqshg <liza.tutak@gmail.com>
2026-08-05 20:34:42 +0300

    5.08

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector("#modal")
+const modalClose = document.querySelector("#modalClose")
+const btnSubmit = document.querySelector("#btnSubmit")
+const back = document.querySelector("#back")
+const input-modal=
+
+modalClose.addEventListener("click", () => {
+    modal.classList.add("modal-close")
+    back.classList.add("modal-close")
+})
+
